fbdc9814d08f0359e3f358ff2443fcced374e72e
avgsteve <steve.leng@gmail.com>
2020-05-14 02:24:42 +0800

    53. Handling POST Requests
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064850
    
    1.
    //設定POST request and add new data
    app.post('/api/v1/tours', (req, res) => {
      //middleware
      console.log("\n=== POST request received! The req.body is:");
      console.log(req.body);
      //give the POST request a "new id" based on the last item's id the array
      const newID = tours[tours.length - 1].id + 1;
    
    2.
      //透過Object.assign將req.body的內容(POST method)存到變數newTour
      const newTour = Object.assign({
        id: newID
      }, req.body);
      /*
      {
      	"name" : "Test tour",
      	"duration" : 10,
      	"difficulty" : "easy"
      }
      */
    
      //將新的newTour資料存入現有的 tours Array 資料中
      tours.push(newTour);
    
      //Asyncronous file writing method
      //fs.writeFile(file, data[, options], callback)
      fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
        // 201 = created
        res.status(201).json({
          status: 'success',
          data: {
            tour: newTour,
          }
        });
      });
    
      // res.send('Done');
      console.log('...New obj created!');
    });

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 /*jshint esversion: 8 */
 const fs = require('fs');
 const express = require('express');
-
 const app = express();
+//app.use(express.json()) ==> middleware: can modify incoming data
+//http://expressjs.com/en/api.html#express
+app.use(express.json());
+
+
 /* REFERENCE
 //http method, when browser sends a get request
 app.get('/', (req, res) => {
@@ -26,10 +30,12 @@ app.post('/', (req, res) => {
 });
 */
 
+//將JSON檔案轉乘物件檔案格式
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//設定GET request 會傳到瀏覽器的內容，傳出的內容為JSON格式的資料
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -38,6 +44,45 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     }
   });
+});
+
+//設定POST request and add new data
+app.post('/api/v1/tours', (req, res) => {
+  //middleware
+  console.log("\n=== POST request received! The req.body is:");
+  console.log(req.body);
+  //give the POST request a "new id" based on the last item's id the array
+  const newID = tours[tours.length - 1].id + 1;
+
+  //透過Object.assign將req.body的內容(POST method)存到變數newTour
+  const newTour = Object.assign({
+    id: newID
+  }, req.body);
+  /*
+  {
+  	"name" : "Test tour",
+  	"duration" : 10,
+  	"difficulty" : "easy"
+  }
+  */
+
+  //將新的newTour資料存入現有的 tours Array 資料中
+  tours.push(newTour);
+
+  //Asyncronous file writing method
+  //fs.writeFile(file, data[, options], callback)
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    // 201 = created
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      }
+    });
+  });
+
+  // res.send('Done');
+  console.log('...New obj created!');
 });
 
 
