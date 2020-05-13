4a9c3d0edb5cd832a084ce35a69a4b6d460df2fd
avgsteve <steve.leng@gmail.com>
2020-05-13 09:21:38 +0800

    50. Setting up Express and Basic Routing (for http GET and POST request)
    ref: https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064844
    
    app's first setup ref:
    http://expressjs.com/en/api.html#app
    
    app.get():
    http://expressjs.com/en/api.html#app.get
    
    app.post():
    http://expressjs.com/en/api.html#app.post.method

--- app.js
+++ app.js
@@ -4,14 +4,31 @@ const express = require('express');
 
 const app = express();
 
-//http method, how to response to request
-app.get('/', (res, req) => {
-  res.status(200).send('Hello from the server side! :)');
-
+//http method, when browser sends a get request
+app.get('/', (req, res) => {
+  //// ==== send message via .send method (see ref: http://expressjs.com/en/api.html#res.status)
+  // res.status(200).send('Hello from the server side! :)');
+  // ==== send .json via .json method
+  res.status(200).json({
+    message: 'Hello from the server side! :)',
+    app: 'Natours',
+  });
+  //// ====
+  // res.status(200).json(req);
+  // console.log(req);
 });
+
+
+//// http method, when browser sends a get request
+// app.post(path, callback [, callback ...])  // ref: http://expressjs.com/en/api.html#app.post.method
+app.post('/', (req, res) => {
+  res.send("This is a response to the POST request! 🧡");
+});
+
+
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...\nThe address is: http://127.0.0.1:${port}`);
 });
