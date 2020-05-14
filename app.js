8a02cddf9c22bba656ed9c4924c94e1fca1ca298
avgsteve <steve.leng@gmail.com>
2020-05-14 18:36:47 +0800

    56. Handling DELETE Requests
    Ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064860
    
    跟 PATCH request 差不多，只是 送出的res.tatus(204)不會送出資料到瀏覽器
    
    ref:  https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
    
    The HTTP 204 No Content success status response code indicates that the request has succeeded, but that the client doesn't need to go away from its current page. A 204 response is cacheable by default. An ETag header is included in such a response.

--- app.js
+++ app.js
@@ -142,6 +142,34 @@ app.patch('/api/v1/tours/:id', (req, res) => {
 
 });
 
+// DELETE request , 將不會送出資料到browser
+app.delete('/api/v1/tours/:id', (req, res) => {
+  //ex: 127.0.0.1:3000/api/v1/tours/5 的GET request 會顯示  "req.params": {"id": "5"}
+
+  console.log('\n===== req.param for DELETE request is:');
+  console.log(req.params);
+
+  //在tours Array 裡面搜尋有key: id跟req.params相符內容，並透過find傳回整個符合條件的 Array
+  const tour = tours.find(el => el.id === +req.params.id); //req.params.id前的+號是coersion為數值
+  console.log(tour === undefined ? `%c invalid id input from URL: ${req.params.id}` : tour); // is a obj
+
+  // to make sure user entered the correct id
+  if (+req.params.id > tours.length || !tour) {
+    //when can't find the correct id
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid id",
+      incorrect_input: req.params,
+    });
+  }
+
+  // status 204 will not send out data to browser , only the status code 204
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
