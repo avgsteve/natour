3395f82fa6cdfe9c3710b2edc08e9229ef36b9c9
avgsteve <steve.leng@gmail.com>
2020-05-18 23:42:04 +0800

    82. Connecting Database with Express and Mongoose
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065060
    
    1. first install
    npm i mongoose
    for mongoose package for connection
    ref:  https://mongoosejs.com/docs/index.html
    
    2. config for connecting to Atlas database like variables including
    
    DATABASE=mongodb ...
    DATABASE_PASSWORD=...
    DATABASE_LOCAL=mongodb://localhost:27017/natours
    
    3.  In server.js, setup package importing and mongoose.connect Promise object for connection
    
    ref:  https://mongoosejs.com/docs/connections.html
    
    ref #2:  https://mongoosejs.com/docs/connections.html#options
    Below are some of the options that are important for tuning Mongoose.
    
    -  useNewUrlParser :
    useNewUrlParser - The underlying MongoDB driver has deprecated their current connection string parser. Because this is a major change, they added the useNewUrlParser flag to allow users to fall back to the old parser if they find a bug in the new parser. You should set useNewUrlParser: true unless that prevents you from connecting. Note that if you specify useNewUrlParser: true, you must specify a port in your connection string, like mongodb://localhost:27017/dbname. The new url parser does not support connection strings that do not have a port, like mongodb://localhost/dbname.

--- server.js
+++ server.js
@@ -3,9 +3,24 @@
 const path = require('path');
 const scriptName = path.basename(__filename);
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({
   path: './config.env'
+});
+
+//use DB and replace password string with env
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+// will return a promise obj
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true // to hide deprication error warbubg message in terminal
+}).then(connection => {
+  //to show connections properties
+  console.log(connection.connections);
+  console.log("\nDB connections successful!  (●'◡'●)");
 });
 
 const app = require("./app"); // getting all config from app.js , so use nodemon server.js to start server
