9ec736c44ef713bfbf6e5642e9bb06eaa1e6b8e8
avgsteve <steve.leng@gmail.com>
2020-05-19 11:01:17 +0800

    85. Creating Documents and Testing the Model (Schema & model)
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065066
    
    1. Create a new class-like "Schema" to be used in mongoose.model
    
    2.Modes (mongoose.model)  :+1:
    ref:  https://mongoosejs.com/docs/models.html
    
    Models are fancy constructors compiled from Schema definitions. An instance of a model is called a document. Models are responsible for creating and reading documents from the underlying MongoDB database.
    
    3. create an instance of the mongoose.model with the passed-in arguments such as instance name and callback "schema" function to defined the structure and rules of new document ( new data to be created )
    
    ex:  const Tour = mongoose.model('Tour', tourSchema);
    
    4. create a new document data with the instance "Tour" for testing
    
    const testTour = new Tour({
      name: 'The Forest Hiker test',
      rating: 4.7,
      price: 999,
    });
    
    5. save the new document with it's obj method .save which will then return a promise obj.
    
    Use .then and .catch method to handle the result of the promise obj

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ mongoose.connect(DB, {
 });
 
 //Create a class-like "Schema" to descript the data
-const tourSchema = new mongoose.schema({
+const Schema = mongoose.Schema;
+//mongoosejs.com/docs/guide.html#models
+
+const tourSchema = new Schema({
   //schema options ref:  https://mongoosejs.com/docs/guide.html#definition
   name: {
     //this type of obj prop is the "schema type option"
@@ -50,6 +53,25 @@ const tourSchema = new mongoose.schema({
 // // make a collection based on the tourSchema by using model constructors function
 // ref:  https://mongoosejs.com/docs/models.html#compiling
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker test',
+  rating: 4.7,
+  price: 999,
+});
+
+//save() ref:  https://mongoosejs.com/docs/api.html#document_Document-save
+testTour.save(
+  //.save() will return a promise
+).then(doc => {
+  console.log("\nNew document saved!:\n");
+  console.log(doc);
+}).catch(err => {
+  console.log("\nOops there's an error!: \n");
+  console.log(err);
+});
+
+
 
 //=======================================================
 
