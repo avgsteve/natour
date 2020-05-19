504c8bee3f472730bc011a8eac769d0a072fe5c2
avgsteve <steve.leng@gmail.com>
2020-05-19 10:09:53 +0800

    84. Creating a Simple Tour Model (mongoose schema types and model)
    1.Schema type option:
    Create a class-like "Schema" to descript the data.
    You can declare a schema type using the type directly, or an object with a type property.
    
    ref:  https://mongoosejs.com/docs/schematypes.html#schematype-options
    
    2. make a collection based on the tourSchema by using model constructors function
    ref:  https://mongoosejs.com/docs/models.html#compiling
    const Tour = mongoose.model('Tour', tourSchema);

--- server.js
+++ server.js
@@ -9,19 +9,50 @@ dotenv.config({
   path: './config.env'
 });
 
+//==============  DATABASE RELATED SECTION  ================
+
 //use DB and replace password string with env
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
-// will return a promise obj
+
+// ------- Mongoose ------------
+
+// will return a promise obj with using Mongoose
 mongoose.connect(DB, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  useUnifiedTopology: true // to hide deprication error warbubg message in terminal
+  useUnifiedTopology: true // to hide deprication error warning message in terminal
 }).then(connection => {
   //to show connections properties
   console.log(connection.connections);
   console.log("\nDB connections successful!  (●'◡'●)");
 });
+
+//Create a class-like "Schema" to descript the data
+const tourSchema = new mongoose.schema({
+  //schema options ref:  https://mongoosejs.com/docs/guide.html#definition
+  name: {
+    //this type of obj prop is the "schema type option"
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: String,
+    required: [true, "A tour  must have a price"],
+  },
+});
+
+// // make a collection based on the tourSchema by using model constructors function
+// ref:  https://mongoosejs.com/docs/models.html#compiling
+const Tour = mongoose.model('Tour', tourSchema);
+
+//=======================================================
+
 
 const app = require("./app"); // getting all config from app.js , so use nodemon server.js to start server
 
