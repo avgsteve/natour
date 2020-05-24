0cc3dad79810f3376dab96bcc7866e1d6d8700b7
avgsteve <steve.leng@gmail.com>
2020-05-25 01:00:42 +0800

    100. Refactoring API Features #1 (refactor Model.methods to one single Class Obj)
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065100   @  13:31 / 15:38
    
    將URL 所傳入的不同的 req.query 次一層物件屬性，例如: req.query.duration ,  req.query.sort,  req.query.fields , req.query.page,   req.query.limit 所相對應的mongoose Query methods 改寫進 Class constructor 裡面的 method，

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,11 +14,132 @@ const tours = JSON.parse(
 );
 */
 
+//refactoring code by using class
+class APIFeatures {
+  constructor(query, queryString) {
+    // First parameter "query" is the query obj from using mongoose's 'Model class' ,
+    // Second parameter "queryString" is the req.query's key and values string from the parsed URL request by express.js
+    this.query = query; //ex: after using Query methods like Tour.find() , Tour.sort() etc,.
+    this.queryString = queryString;
+    // captured query string parameter from HTTP requests. Ex: req.query.page, req.query.field etc,.
+  }
+
+  //APIfeatures.filter()
+  filter() {
+    const queryObj = {
+      ...this.queryString
+      // ex:   duration: { gte: '5' },   fields: 'name,duration,difficulty,price'
+    };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // // making the processed JSON strings back to Obj format and save it back to this.query
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this; //makes APIfeatures.filter() has props and value of the whole updated Obj
+  }
+
+  //APIfeatures.sort()
+  sort() {
+    // // 2) SORTING ( with mongoose Model.find().sort() )
+    if (this.queryString.sort) {
+      /*  req.query.sort obj examples
+      //ex: http://127.0.0.1:3000/api/v1/tours?sort=-price,ratingsAverage
+      //then the sorting string of req.query.sort will be : { sort: '-price,ratingsAverage' }
+      */
+
+      // reformat the sorting string (value of this.queryString.sort, ex: sort: '-price,ratingsAverage')
+      // from sort: "x,y" to "x y" to match the format of arguments for .sort()
+      const newSortingQuery = this.queryString.sort.split(",").join(" ");
+
+      console.log('\nReformated sorting string from "req.query.sort"is :  ' + newSortingQuery + '\n');
+      /* sorting method examples :
+    //  **** sort by "field" ascending and "test" descending
+    query.sort({ field: 'asc', test: -1 });
+    //  **** equivalent
+    query.sort('field -test');
+*/
+      //update the this.query with a new query after executing method of ".sort(string)"
+      this.query = this.query.sort(newSortingQuery);
+      //ref:  https://mongoosejs.com/docs/api/query.html#query_Query-sort
+
+    } else {
+      //provide a default sorting string
+      this.query = this.query.sort('price');
+    }
+
+    return this; // return the whole updated class instance itself
+
+  }
+
+  //APIfeatures.limit()
+  limitFields() {
+    // 3) Field limiting (projecting) by using .select() method
+    // (to send back only required key-value to reduce the size of requested data)
+    if (this.queryString.fields) {
+
+      const selectedFields = this.queryString.fields.split(",").join(" ");
+
+      console.log('\nString from req.query.fields: \n  ==>' + this.queryString.fields + "\n reformated to :  \n --->" + selectedFields + "\n");
+
+      this.query = this.query.select(selectedFields);
+
+      //ref:  https://mongoosejs.com/docs/api/query.html#query_Query-select
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    // 4) Pagination (by using .skip() and .limit() methods )
+
+    // Set default value of page from the .page property
+    const page = +this.queryString.page || 1;
+    // To pre-define the limit of maximun number of results to be returned
+    const limit = +this.queryString.limit || 100;
+    // how many documents to skip in the result, page 2 will skip the number of limit multiplied by the page before (which is page - 1)
+    const skip = (page - 1) * limit;
+
+    console.log(`\x1b[31m\nCurrent query output limit is : ${limit}\x1b[0m,\nCurrent page is #${page} and there are ${skip} of the results have been skipped`);
+
+    // Decide how many results to skip and the maximun number of results
+    // ex: page 2 is result #11 to #20 -->
+    this.query = this.query.skip(skip).limit(limit);
+    /* ref:
+    //page=2&limit=10, 1-10 is page 1, 11-20 is page 2,
+    // .skip:  https://mongoosejs.com/docs/api/query.html#query_Query-skip
+    // .limit:  https://mongoosejs.com/docs/api/query.html#query_Query-limit
+*/
+
+    /*  (No need to throw an error when user gets NO data if the user uses too much number of pages )
+
+        if (this.queryString.page) {
+
+          // .countDocuments() will return how many results from the query
+          const numOfTourResults = this.query.countDocuments(); //will return a query Promise obj
+
+          // ref:  https://mongoosejs.com/docs/api/model.html#model_Model.countDocuments
+
+          if (skip >= numOfTourResults) throw new Error('The number of skip is greater than query numbers');
+
+          // the new Error message in try { ... } method will be moved on to catch { } block as error message and trigger status 404 content immediately
+
+        }
+    */
+    return this;
+
+  }
+
+}
+
 // 2) ============== ROUTE-HANDLERS
-
-
 // pre-filling the query objs before using next middleware (getAllTours)
-exports.aliasTopTours = async (req, res, next) => {
+exports.aliasTopTours = (req, res, next) => {
   // for touRoutes.js => router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
@@ -27,152 +148,48 @@ exports.aliasTopTours = async (req, res, next) => {
   next(); // which is tourController.getAllTours
 };
 
-
 exports.getAllTours = async (req, res) => {
-  //using newly create middleware function to log time
+  //log current file name and time from
   console.log(`\n(from ${scriptName}: ) The requested was made at ${req.requestTime}`);
-
   console.log("\x1b[93m", "\nThe req.query obj from the GET request:", "\x1b[0m\n");
-
   console.log(req.query);
 
   // { difficulty: 'easy', duration: { gte: '5' } }
 
   try {
 
-    // ============ #1 BUILD THE QUERY ============
-
-    // // 1A) Filtering
-    //make a shallow copy of req.query
-    const queryObj = {
-      ...req.query
-    };
-
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    //deleting the excluded fields (properties) from the queryObj and makes queryObj a new one
-    excludedFields.forEach(propertyItem => delete queryObj[propertyItem]);
-
-    // // 1B) Advanced Filtering
-
-    // // 將 URL query ，ex:
-    // http://127.0.0.1:3000/api/v1/tours?duration[gte]=5 傳入的物件 value  { duration: { 'gte': '6' } } 改寫為  { '$gte': '6' }  來作為傳入 .find() method 的 argument
-
-
-    //convert js object to string as JSON(both key and value)
-    let queryStr = JSON.stringify(queryObj);
-    console.log('\nJSON.stringify(queryObj);');
-    console.log(queryStr);
-
-    // gte, gt, lte, lt ==> $gte, $gt, $lte, $lt
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    //
-    console.log('\nreplaced string (queryStr):');
-    console.log(queryStr);
-
-    queryStrReplaced = JSON.parse(queryStr);
-    console.log('\nafter JSON.parse(queryStrReplaced)');
-    console.log(queryStrReplaced);
-
-
-    let newQuery = Tour.find(queryStrReplaced);
-
-    // // 2) SORTING ( with mongoose Model.find().sort() )
-    if (req.query.sort) {
-      /*  req.query.sort obj examples
-      //ex: http://127.0.0.1:3000/api/v1/tours?sort=-price
-      //then the sorting string of req.query.sort will be : { sort: '-price' }
-      //ex: http://127.0.0.1:3000/api/v1/tours?sort=-price,ratingsAverage
-      //then the sorting string of req.query.sort will be : { sort: '-price,ratingsAverage' }
-      */
-
-      // reformat the sorting string from sort: "x,y" to "x y" to match the format of arguments for .sort()
-      const newSortingQuery = req.query.sort.split(",").join(" ");
-      console.log('\nReformated sorting string from "req.query.sort"is :  ' + newSortingQuery + '\n');
-      /* sorting method examples :
-      //  **** sort by "field" ascending and "test" descending
-      query.sort({ field: 'asc', test: -1 });
-      //  **** equivalent
-      query.sort('field -test');
-      //ref:  https://mongoosejs.com/docs/api/query.html#query_Query-sort
-*/
-      newQuery = newQuery.sort(newSortingQuery);
-    } else {
-      //provide a default sorting string
-      newQuery = newQuery.sort('price');
-    }
-
-    // 3) Field limiting (projecting) by using .select() method
-    // (to send back only required key-value to reduce the size of requested data)
-    if (req.query.fields) {
-
-      const selectedFields = req.query.fields.split(",").join(" ");
-
-      console.log('\nString from req.query.fields: \n  ==>' + req.query.fields + "\n reformated to :  \n --->" + selectedFields + "\n");
-
-      newQuery = newQuery.select(selectedFields);
-
-      //ref:  https://mongoosejs.com/docs/api/query.html#query_Query-select
-    } else {
-      newQuery = newQuery.select('-__v');
-    }
-
-
-    // 4) Pagination (by using .skip() and .limit() methods )
-
-    // Set default value of page from the .page property
-    const page = +req.query.page || 1;
-    // To pre-define the limit of maximun number of results to be returned
-    const limit = +req.query.limit || 100;
-    // how many documents to skip in the result, page 2 will skip the number of limit multiplied by the page before (which is page - 1)
-    const skip = (page - 1) * limit;
-
-    console.log(`\x1b[31m\nCurrent query output limit is : ${limit}\x1b[0m,\nCurrent page is #${page} and there are ${skip} of the results have been skipped`);
-
-    // Decide how many results to skip and the maximun number of results
-    // ex: page 2 is result #11 to #20 -->
-    newQuery = newQuery.skip(skip).limit(limit);
-    /* ref:
-    //page=2&limit=10, 1-10 is page 1, 11-20 is page 2,
-    // .skip:  https://mongoosejs.com/docs/api/query.html#query_Query-skip
-    // .limit:  https://mongoosejs.com/docs/api/query.html#query_Query-limit
-*/
-
-
-    if (req.query.page) {
-      // .countDocuments() will return how many results from the query
-      const numOfTourResults = await Tour.countDocuments(); //will return a query Promise obj
-      // ref:  https://mongoosejs.com/docs/api/model.html#model_Model.countDocuments
-
-      if (skip >= numOfTourResults) throw new Error('The number of skip is greater than query numbers'); // the new Error message in try { ... } method will be moved on to catch { } block as error message and trigger status 404 content immediately
-    }
+    //將所有
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate(); //To use APIfeatures. filter method
 
     // #2 ============  EXECUTE QUERY  ============
-    const tourResults = await newQuery;
+
+    // (before refactoring the code) const tourResults = await newQuery;
+    const tourResults = await features.query;
+
+    /*
+        // get all current data from DB
+        // const tours = await Tour.find(); //ref:  https://mongoosejs.com/docs/api.html#model_Model.find
+
+        // // --- different ways of querying data
+        // // // 1. monogoDB way
+        // const tours = await Tour.find({
+        //   duration: 5,
+        //   difficulty: "easy",
+        // });
+
+        // this method has the same result as #1 monogoDB way
 
 
-    // get all current data from DB
-    // const tours = await Tour.find(); //ref:  https://mongoosejs.com/docs/api.html#model_Model.find
+        // const queryWith_queryStr = await Tour.find(newQuery);
 
-    // // --- different ways of querying data
-    // // // 1. monogoDB way
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: "easy",
-    // });
-
-    // this method has the same result as #1 monogoDB way
-
-
-    // const queryWith_queryStr = await Tour.find(newQuery);
-
-    // // // 2. mongooseB way
-    // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
-    // ref: https://mongoosejs.com/docs/api/query.html#query_Query-where
-
+        // // // 2. mongooseB way
+        // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+        // ref: https://mongoosejs.com/docs/api/query.html#query_Query-where
+    */
 
     res.status(200).json({
       status: 'success',
-      requestedAt: req.requestTime,
+      requestedAt: req.requestTime, //from app.js => req.requestTime = new Date().toISOString();
       results: tourResults.length,
       data: {
         tours: tourResults,
