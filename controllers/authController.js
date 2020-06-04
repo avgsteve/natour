cd8e7587261f56fa8d555e8ab36b91c09fc85375
avgsteve <steve.leng@gmail.com>
2020-06-05 00:06:19 +0800

    128. Signing up Users (encrypt user id with JWT)
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065292

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,36 @@
 /*jshint esversion: 6 */
 /*jshint esversion: 8 */
 const User = require('./../models/userModel');
-
+const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 //
-exports.signup = async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
+  //
+  // const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   //
-  const newUser = await User.create(req.body);
+  // jwt.sign(payload, secretOrPrivateKey, [options, callback])
+  const token = jwt.sign({
+      //first argument : payload:
+      id: newUser._id // from MongoDB's _id property;
+      //to decode  the id:  https://jwt.io/
+    },
+    //second argument : secretOrPrivateKey
+    process.env.JWT_SECRET,
+    // fast way to generate quick and easy way to generate JWT secret. In terminal: node -e "console.log(require('crypto').randomBytes(64).toString('hex'));"
+    //ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065292#questions/8159650
+
+    { //third argument
+      expiresIn: process.env.JWT_EXPIRES_IN
+    }
+  );
+
 
   /*
   {
@@ -16,14 +40,13 @@ exports.signup = async (req, res, next) => {
   	"passwordConfirm": "pass123"
   }
   */
-
   //
   res.status(201).json({
     status: 'success',
+    token: token,
     data: {
       user: newUser,
     },
 
   });
-
-};
+});
