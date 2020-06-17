bf1b82dfa55e6c400068af010bbaba09f5ec1ee5
avgsteve <steve.leng@gmail.com>
2020-06-17 12:39:46 +0800

    154. Creating and Getting Reviews #2 (adjust code format)

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,33 +1,38 @@
 /*jshint esversion: 6 */
 /*jshint esversion: 8 */
-const Review = require('./../models/reviewModel');
-const catchAsync = require('./../utils/catchAsync');
-
+const Review = require("./../models/reviewModel");
+const catchAsync = require("./../utils/catchAsync");
+const logCurrentModuleName = require("./../utils/getFunctionName");
 
 //
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+	// DisplayCurrentFunctionName();
 
-  res.status(200).json({
-    status: 'successfully get all reviews',
-    results: reviews.length,
-    data: {
-      reviews: reviews
-    }
-  });
+	// console.log(this); //{ getAllReviews: [Function], createReviews: [Function] }
 
+	console.log(`Current Module name is: ${Object.keys(this)[0]}`); //	['getAllReviews', 'createReviews']
+
+	// logCurrentModuleName();
+
+	const reviews = await Review.find();
+
+	res.status(200).json({
+		status: "successfully get all reviews",
+		results: reviews.length,
+		data: {
+			reviews: reviews
+		}
+	});
 });
-
 
 //
 exports.createReviews = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create(req.body);
+	const newReview = await Review.create(req.body);
 
-  res.status(201).json({
-    status: 'new review successfully created!',
-    data: {
-      newReview: newReview
-    }
-  });
-
+	res.status(201).json({
+		status: "new review successfully created!",
+		data: {
+			newReview: newReview
+		}
+	});
 });
