e7ae6327f14c4a3f13bc9724c13adad2dd80886b
avgsteve <steve.leng@gmail.com>
2020-06-28 23:46:51 +0800

    181. Building the Tour Overview - Part 1 (get data from viewsController and render in overview.pug)
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065636
    
    1. In  views\overview.pug,
    
          //- Use built-in loop function "each" to loop through the tours data array and render results to html
    
          each tour in tours
    
    See the file  :  public\tour-dup.html
    for the rendered HTML code

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,14 +1,28 @@
 /*jshint esversion: 6 */
 /*jshint esversion: 8 */
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
 //
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection (from database)
+  const tours = await Tour.find();
+
+  console.log("\nthe tours data from exports.getOverview\n");
+  console.log(tours);
+
+
+  // 2) Build template
+
+
+  // 3) Render that template using tour data from 1)
 
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours: tours
   });
 
-};
+});
 
 //
 exports.getTour = (req, res) => {
