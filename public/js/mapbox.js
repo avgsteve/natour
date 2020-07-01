5e569c8de7414c0e74649da1d08cc55f8e52e283
avgsteve <steve.leng@gmail.com>
2020-07-01 22:27:26 +0800

    186. Including a Map with Mapbox - Part 2 (use mapbox in HTML)
    ref: https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065656  @  8:18 / 27:51
    
    1. Get access token for using mapbox:  https://account.mapbox.com/
    
    2. Create customized token:  https://account.mapbox.com/access-tokens/create
    
    3. (In views\tour.pug) Implement cdn in head in HTML:
    
      script(src='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.js')
      link(href='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css' rel='stylesheet')
    
    4. Use div with id: 'map' to render map in browser as below:
    
    <div id='map' style='width: 400px; height: 300px;'></div>
    
    The script will append a child div with <canvas> to render map in browser:
    
    <div class="mapboxgl-canvas-container mapboxgl-interactive mapboxgl-touch-drag-pan mapboxgl-touch-zoom-rotate">
        <canvas class="mapboxgl-canvas" tabindex="0" aria-label="Map" width="2470"
        height="1832" style="width: 876px; height: 650px;"></canvas>
    </div>
    
    5. Use script file before </body> for config the Mapbox item:
    <script src="/js/mapbox.js"></script>

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -8,11 +8,34 @@ console.log(`=== Log from mapbox.js ===\n`);
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
 console.log(locations);
-// const locations = document.getElementById('map');
 
-// const locations = JSON.stringify(document.getElementById('map').dataset.locations);
+/* Note: in the tour.pug file, this code:
+    section.section-map
+      #map(data-location=`${JSON.stringify(tour.locations)}`)
 
-/* Note: in the tour.pug file:
-section.section-map
-  #map(data-location=`${JSON.stringify(tour.locations)}`)
+  will set "data-location" attribute like below:
+    <section class="section-map"><div id="map" data-locations="[{&quot;type&quot;:&quot;Point&quot;,&quot;coordinates&quot
 */
+
+
+
+// ==== using Mapbox in HTML file ====
+// Make to use script file before </body> for configurating the Mapbox map first:
+// <script src="/js/mapbox.js"></script>
+
+// Using mapbox with CDN and the code from instruction on www.mapbox.com
+// ref:  https://www.mapbox.com/install/js/cdn-add/
+
+//    --==== Unique token for rendering map in page
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXZnc3RldmUiLCJhIjoiY2tjM2Y0cXBvMmN5bDM0bXh5bmFxbjBrMiJ9.1foja_AwKRS0p_Xrn-zX1g';
+// creating new tokens for different project: https://account.mapbox.com/access-tokens
+
+//    --==== configuration for Mapbox map
+var map = new mapboxgl.Map({
+  container: 'map',
+
+  // style property for using default or customized map styles
+  style: 'mapbox://styles/avgsteve/ckc3fgxvn0svq1itaflci74ke'
+  // Get map styling code: https://studio.mapbox.com/
+  // -> Share options -> Developer resources : "Web" -> Style URL
+});
