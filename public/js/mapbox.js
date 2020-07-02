bc1aa1ad556969444bf5717dc37d1b0c6fb8da6f
avgsteve <steve.leng@gmail.com>
2020-07-02 12:26:54 +0800

    186.  Set customized marker, map padding & panning effect
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065656
    
    1-1. Set marker on map and area to be displayed
    
    // Set the area will be displayed on the map by creating new mapboxgl.LatLngBounds(); Obj
    // https://docs.mapbox.com/mapbox-gl-js/api/geography/#lnglatbounds
    
    // If no arguments are provided to the constructor, a null bounding box is created.
    
    Code:
    +  new mapboxgl.Marker({
          element: nodeElement,
    
          //Set the bottom of the pin as the exact geolocation (like an anchor)
          anchor: 'bottom',
    
          // ref: https://docs.mapbox.com/help/tutorials/custom-markers-gl-js/#add-markers-to-the-map
    
        })
    
    1-2. Use chainable method after new mapboxgl.Marker({ ... }) obj
    
        This set the coordinates (which is read from each location item from "locations" Array) for the marker
    
        Code:
        .setLngLat(singleLocation.coordinates)
    
        Then add (attach) the marker object to map obj
        Code:
        .addTo(map);
    
    //ref: https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker#addto
    
    2-1. Set bound (display area) for all given coordinates
    
    const bounds = new mapboxgl.LngLatBounds();
    
    Note: This LngLatBounds obj will be worked with the code: bounds.extend(singleLocation.coordinates); to set bound (display area) for all given coordinates
    
    2-2. Which looping through all locations and getting coordinates data from each item, use bounds.extend() to set boundary of displayed map
    
      Code:
      bounds.extend(singleLocation.coordinates);
    
    3. Pans and zooms the map to contain its visible area within the specified geographical bounds. This function will also reset the map's bearing to 0 if bearing is nonzero.  ref: https://docs.mapbox.com/mapbox-gl-js/api/map/#map#fitbounds
    
    (map object was created from the code:
      var map = new mapboxgl.Map( ... )  )
    
    map.fitBounds(bounds obj (as 2-1.), options)
    
    2-2: Padding in map.fitBounds obj ( as argument object)
    
      ref:  ref: https://docs.mapbox.com/mapbox-gl-js/api/properties/#paddingoptions-example
    
      ex:
      {
        padding: {
          //units: px
          top: 200,
          bottom: 200,
          left: 100,
          right: 100
        }
      }

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -3,9 +3,44 @@
 
 console.log(`=== Log from mapbox.js ===\n`);
 
-//Get the element with id: map and the value in dataset.location
-//Then parse the JSON format data into JS Object format
+// Get the coordinates value in dataset.location from the element with id: map
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
+//Then parse the JSON format data into JS Object format
+
+/* ### The sample Object content (from variable "locations") ###
+[
+  {
+    "type": "Point",
+    "coordinates": [
+      -73.967696,
+      40.781821
+    ],
+    "_id": "5c88fa8cf4afda39709c2960",
+    "description": "New York",
+    "day": 1
+  },
+  {
+    "type": "Point",
+    "coordinates": [
+      -118.324396,
+      34.097984
+    ],
+    "_id": "5c88fa8cf4afda39709c295f",
+    "description": "Los Angeles",
+    "day": 3
+  },
+  {
+    "type": "Point",
+    "coordinates": [
+      -122.408865,
+      37.787825
+    ],
+    "_id": "5c88fa8cf4afda39709c295e",
+    "description": "San Francisco",
+    "day": 5
+  }
+]
+*/
 
 console.log(locations);
 
@@ -47,11 +82,86 @@ var map = new mapboxgl.Map({
   // set default map location
   // ref:  https://docs.mapbox.com/mapbox-gl-js/example/attribution-position/index.html
   center: [-77.04, 38.907],
-  zoom: 8,
+  zoom: 15,
 
   // to make map static (can't be dragged. Will look like a photo on page)
   // interactive: false
 
 });
 
+//
 map.addControl(new mapboxgl.AttributionControl(), 'top-left');
+
+
+// === Set marker on map and area to be displayed ===
+
+// Set the area will be displayed on the map by creating new mapboxgl.LatLngBounds(); Obj
+// https://docs.mapbox.com/mapbox-gl-js/api/geography/#lnglatbounds
+
+// If no arguments are provided to the constructor, a null bounding box is created.
+const bounds = new mapboxgl.LngLatBounds();
+
+
+// Look for coordinates in each singleLocation to create marker on map by looping through the locations data Array
+locations.forEach(singleLocation => {
+
+  // create and node element for adding marker later
+  // note: CSS for Marker: style.css --> .marker {}
+  const nodeElement = document.createElement('div');
+  nodeElement.className = 'marker';
+
+  // make new marker Obj.  ref: Markers and controls
+  // https://docs.mapbox.com/mapbox-gl-js/api/markers/
+  new mapboxgl.Marker({
+      element: nodeElement,
+
+      //Set the bottom of the pin as the exact geolocation (like an anchor)
+      anchor: 'bottom',
+
+      // ref: https://docs.mapbox.com/help/tutorials/custom-markers-gl-js/#add-markers-to-the-map
+
+    })
+
+    // (chainable methods)
+    // Then set the coordinates (which is read from each location item from "locations" Array) for the marker
+    .setLngLat(singleLocation.coordinates)
+
+    //Then add (attach) the marker object to map obj
+    .addTo(map); //ref: https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker#addto
+
+
+  // === bounds.extend: ===
+  // Extend map bounds to include current location
+  // ref: https://docs.mapbox.com/mapbox-gl-js/api/geography/#lnglatbounds#extend
+
+  // parameters: obj((LngLatLike | LngLatBoundsLike)) .  ref: https://docs.mapbox.com/mapbox-gl-js/api/geography/#lnglatlike
+
+  bounds.extend(singleLocation.coordinates);
+
+  // End of locations.forEach(singleLocation => {
+});
+
+
+// ==== Panning map location to another ===
+
+// Use fitBounds to show a specific area of the map in view, regardless of the pixel size of the map.  ref: ref: https://docs.mapbox.com/mapbox-gl-js/example/fitbounds/
+
+// Pans and zooms the map to contain its visible area within the specified geographical bounds. This function will also reset the map's bearing to 0 if bearing is nonzero.  ref: https://docs.mapbox.com/mapbox-gl-js/api/map/#map#fitbounds
+
+// Basic fitBounds code:  map.fitBounds(bounds);
+
+// fitBounds code with arguments and parameters.
+// ref:  https://docs.mapbox.com/mapbox-gl-js/api/map/#fitbounds-parameters
+map.fitBounds(bounds,
+
+  //Create paddings for the marker displayed on map by passing in an "padding" obj as argument. ref: https://docs.mapbox.com/mapbox-gl-js/api/properties/#paddingoptions-example
+  {
+    padding: {
+      //units: px
+      top: 200,
+      bottom: 200,
+      left: 100,
+      right: 100
+    }
+  }
+);
