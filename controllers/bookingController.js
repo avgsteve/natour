68613ed41b2145d61333649348aaa85b12c4406c
avgsteve <steve.leng@gmail.com>
2020-07-11 10:31:38 +0800

    215. Finishing the Bookings API
    ref:  https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15087386

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -101,3 +101,10 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
 
 });
+
+
+exports.createBooking = factory.createOne(Booking);
+exports.getOneBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,11 +5,27 @@ const express = require('express');
 const authController = require(`./../controllers/authController`);
 
 const router = express.Router();
-// In app.js,
+//  ==== In app.js, ====
 // app.use('/api/v1/bookings', bookingRouter);
 
+router.use(authController.protect);
+
 //router is for user to get session only
-router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router.route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router.route('/:id')
+  .get(bookingController.getOneBooking)
+  .post(bookingController.createBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
+
 
 
 module.exports = router;
